0a73757587bd0f5768500fbd3a88a2fdc3415a64
lamhoang1256 <lamhoang1256@gmail.com>
2022-10-06 09:01:03 +0700

    Style: responsive chapter list at comic detail page

--- pages/comic/[slug].tsx
+++ pages/comic/[slug].tsx
@@ -23,7 +23,9 @@ const ComicDetailsPage = ({ info, chapters }: ComicDetailsPageProps) => {
   return (
     <>
       <Head>
-        <title>Trang chi tiết truyện</title>
+        <title>
+          {info.title} [Tới Chap {chapters[0].title}] Tiếng Việt
+        </title>
         <meta name="description" content="Trang chi tiết truyện" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -33,77 +35,73 @@ const ComicDetailsPage = ({ info, chapters }: ComicDetailsPageProps) => {
             <div className="lg:w-2/3">
               <div className="text-center">
                 <h1 className="text-[21px] font-semibold uppercase">{info?.title}</h1>
-                <span className="inline-block mt-1 opacity-70 text-[13px] italic">
-                  {info.updatedAt}
-                </span>
+                <span className="inline-block mt-1 italic opacity-70">{info.updatedAt}</span>
               </div>
               <div className="md:flex gap-x-[15px] mt-5">
                 <div className="md:w-[30%] mx-auto aspect-[2/3] w-[180px]">
                   <picture>
                     <source srcSet={info.posterUrl} type="image/webp" />
-                    <img alt={info.title} src={info.posterUrl} className="object-cover rounded " />
+                    <img alt={info.title} src={info.posterUrl} className="object-cover rounded" />
                   </picture>
                 </div>
-                <div className="md:w-[70%]">
-                  <ul>
-                    <li className="flex mb-2 text-base">
-                      <div className="w-1/3">Tác giả</div>
-                      <div className="flex-1">{info.author}</div>
-                    </li>
-                    <li className="flex mb-2 text-base">
-                      <div className="w-1/3">Tình trạng</div>
-                      <div className="flex-1">{info.status}</div>
-                    </li>
-                    <li className="flex mb-2 text-base">
-                      <div className="w-1/3">Thể loại</div>
-                      <div className="flex flex-wrap flex-1 gap-x-2 gap-y-1">
-                        {info.categories.split(" - ").map((category) => (
-                          <Link href="/" key={category}>
-                            <a className="transition-all duration-200 px-[6px] py-[2px] text-blue29 hover:text-redf6 border-blue29 hover:border-redf6 border rounded-[3px] text-sm">
-                              {category}
-                            </a>
-                          </Link>
-                        ))}
-                      </div>
-                    </li>
-                    <li className="flex mb-2 text-base">
-                      <div className="w-1/3">Lượt xem</div>
-                      <div className="flex-1">{info.viewCount}</div>
-                    </li>
-                    <li>
-                      <Link href={`${PATH.comic}/${info.posterUrl}`}>
-                        <a className="mr-2 transition-all duration-200 text-[#0073f4] hover:text-[#044aa4]">
-                          {info.title}
-                        </a>
-                      </Link>
-                      <span className="text-redf6">
-                        Xếp hạng: {info.ratingValue}/5 - {info.ratingCount} Lượt đánh giá.
-                      </span>
-                    </li>
-                    <li className="flex items-center mt-[10px] gap-x-2">
-                      <button className="px-[14px] py-[6px] bg-[#5cb85c] hover:opacity-80 transition-all duration-200 text-white rounded">
-                        Theo dõi
-                      </button>
-                      <span className="text-[15px]">
-                        <b className="text-black">{info.followCount}</b> Lượt theo dõi
-                      </span>
-                    </li>
-                    <li className="flex items-center mt-[10px] gap-x-1">
-                      <button className="px-[14px] py-[6px] bg-[#ff6a3c] hover:opacity-80 transition-all duration-200 text-white rounded">
-                        Đọc từ đầu
-                      </button>
-                      <button className="px-[14px] py-[6px] bg-[#f0ad4e] hover:opacity-80 transition-all duration-200 text-white rounded">
-                        Đọc mới nhất
-                      </button>
-                      <button className="px-[14px] py-[6px] bg-[#0079f7] hover:opacity-80 transition-all duration-200 text-white rounded">
-                        Thích
-                      </button>
-                      <button className="px-[14px] py-[6px] bg-[#dc3545] hover:opacity-80 transition-all duration-200 text-white rounded">
-                        Đọc tiếp
-                      </button>
-                    </li>
-                  </ul>
-                </div>
+                <ul className="md:w-[70%]">
+                  <li className="flex mb-2 text-base">
+                    <div className="w-1/3">Tác giả</div>
+                    <div className="flex-1">{info.author}</div>
+                  </li>
+                  <li className="flex mb-2 text-base">
+                    <div className="w-1/3">Tình trạng</div>
+                    <div className="flex-1">{info.status}</div>
+                  </li>
+                  <li className="flex mb-2 text-base">
+                    <div className="w-1/3">Thể loại</div>
+                    <div className="flex flex-wrap flex-1 gap-x-2 gap-y-1">
+                      {info.categories.split(" - ").map((category) => (
+                        <Link href="/" key={category}>
+                          <a className="transition-all duration-200 px-[6px] py-[2px] text-blue29 hover:text-redf6 border-blue29 hover:border-redf6 border rounded-[3px] text-sm">
+                            {category}
+                          </a>
+                        </Link>
+                      ))}
+                    </div>
+                  </li>
+                  <li className="flex mb-2 text-base">
+                    <div className="w-1/3">Lượt xem</div>
+                    <div className="flex-1">{info.viewCount}</div>
+                  </li>
+                  <li>
+                    <Link href={`${PATH.comic}/${info.posterUrl}`}>
+                      <a className="mr-2 transition-all duration-200 text-[#0073f4] hover:text-[#044aa4]">
+                        {info.title}
+                      </a>
+                    </Link>
+                    <span className="text-redf6">
+                      Xếp hạng: {info.ratingValue}/5 - {info.ratingCount} Lượt đánh giá.
+                    </span>
+                  </li>
+                  <li className="flex items-center mt-[10px] gap-x-2">
+                    <button className="px-[14px] py-[6px] bg-[#5cb85c] hover:opacity-80 transition-all duration-200 text-white rounded">
+                      Theo dõi
+                    </button>
+                    <span className="text-[15px]">
+                      <b className="text-black">{info.followCount}</b> Lượt theo dõi
+                    </span>
+                  </li>
+                  <li className="flex flex-wrap items-center mt-[10px] gap-1">
+                    <button className="px-[14px] py-[6px] bg-[#ff6a3c] hover:opacity-80 transition-all duration-200 text-white rounded">
+                      Đọc từ đầu
+                    </button>
+                    <button className="px-[14px] py-[6px] bg-[#f0ad4e] hover:opacity-80 transition-all duration-200 text-white rounded">
+                      Đọc mới nhất
+                    </button>
+                    <button className="px-[14px] py-[6px] bg-[#0079f7] hover:opacity-80 transition-all duration-200 text-white rounded">
+                      Thích
+                    </button>
+                    <button className="px-[14px] py-[6px] bg-[#dc3545] hover:opacity-80 transition-all duration-200 text-white rounded">
+                      Đọc tiếp
+                    </button>
+                  </li>
+                </ul>
               </div>
               <div className="mt-5">
                 <Heading className="!text-base font-light border-b-2 border-b-blue29">
@@ -117,9 +115,9 @@ const ComicDetailsPage = ({ info, chapters }: ComicDetailsPageProps) => {
                 </Heading>
                 <div className="mt-4">
                   <div className="flex text-base font-semibold">
-                    <span className="w-1/2">Số chương</span>
-                    <span className="w-1/4 text-center">Cập nhật</span>
-                    <span className="w-1/4 text-center">Lượt xem</span>
+                    <span className="w-5/12 md:w-1/2">Số chương</span>
+                    <span className="w-4/12 text-center md:w-1/4">Cập nhật</span>
+                    <span className="w-3/12 text-center md:w-1/4">Lượt xem</span>
                   </div>
                   <ul className="border border-[#080808] pt-[6px] pr-[6px] pl-[10px] rounded mt-[10px] pb-4">
                     {chapters.slice(0, countChapters).map((chapter) => (
@@ -127,11 +125,11 @@ const ComicDetailsPage = ({ info, chapters }: ComicDetailsPageProps) => {
                         className="py-[6px] flex border-b border-dotted border-[#bdbcbc5c]"
                         key={chapter.id}
                       >
-                        <span className="w-1/2">{chapter.title}</span>
-                        <span className="w-1/4 text-center italic text-[#c0c0c0]">
+                        <span className="w-5/12 md:w-1/2">{chapter.title}</span>
+                        <span className="w-4/12 md:w-1/4 text-center italic text-[#c0c0c0]">
                           {chapter.updatedAt}
                         </span>
-                        <span className="w-1/4 text-center italic text-[#c0c0c0]">
+                        <span className="w-3/12 md:w-1/4 text-center italic text-[#c0c0c0]">
                           {chapter.viewCount}
                         </span>
                       </li>
