74813d8d196332d608873a2a84bd93a820a6dc1d
lamhoang1256 <lamhoang1256@gmail.com>
2022-10-07 18:34:02 +0700

    Chore: test server.ts

--- configs/server.ts
+++ configs/server.ts
@@ -1,3 +1,6 @@
 const dev = process.env.NODE_ENV !== "production";
-
 export const server = dev ? "http://localhost:3000" : process.env.SERVER;
+console.log("NODE_ENV: ", process.env.NODE_ENV);
+console.log("dev: ", dev);
+console.log("server: ", server);
+console.log("SERVER: ", process.env.SERVER);
