e50a927e4f6c7493b485c0157a1463a8fef83853
lamhoang1256 <lamhoang1256@gmail.com>
2022-10-07 15:56:44 +0700

    Chore: test api part 3

--- pages/api/comic/[slug]/[chapter]/[id].ts
+++ pages/api/comic/[slug]/[chapter]/[id].ts
@@ -15,51 +15,53 @@ const ChapterComicApi = async (req: NextApiRequest, res: NextApiResponse) => {
     const error = new ApiError(STATUS.METHOD_NOT_ALLOWED, "Method not allowed");
     return responseError(error, res);
   }
-  const data = await getDetailsChapter(`${PATH.netTruyenComic}/${slug}/${chapter}/${id}`);
+  const data: any = await getDetailsChapter(`${PATH.netTruyenComic}/${slug}/${chapter}/${id}`);
+  const chapters = await getLinkChapters(
+    `${PATH.netTruyenComic}/${data?.info.href?.replace("truyen-tranh/", "")}`
+  );
   const response = {
     message: "Lấy chi tiết chapter thành công!",
-    data,
+    data: { ...data, ...chapters },
   };
   responseSuccess(res, response);
 };
 
 const getDetailsChapter = async (url: string) => {
-  // const response = await axios.get(url);
-  // const html = response.data;
-  // const $ = cheerio.load(html);
-  // let imageUrls: IImageReading[] = [];
-  // let info = {} as IDetailsChapter;
-  // let comments: IComment[] = [];
+  const response = await axios.get(url);
+  const html = response.data;
+  const $ = cheerio.load(html);
+  let imageUrls: IImageReading[] = [];
+  let info = {} as IDetailsChapter;
+  let comments: IComment[] = [];
+  $(".reading .container .top")
+    .first()
+    .each(function (index, element) {
+      const originalUrl = PATH.netTruyen + "/";
+      const heading = $(element).find("h1.txt-primary");
+      const href = heading.find("a").attr("href")?.replace(originalUrl, "") as string;
+      const title = heading.find("a").text();
+      const chapter = heading.find("span").text()?.replace("- ", "") as string;
+      const updatedAt = $(element).find("i").text();
+      info = { title, updatedAt, chapter, href };
+    });
+  $(".reading-detail .page-chapter").each(function (index, element) {
+    const imageUrl = getImagesReading($(element));
+    imageUrls.push(imageUrl);
+  });
+  $(".comment-list .item.clearfix").each(function (index, element) {
+    const comment = crawlComments($(element), $);
+    comments.push(comment);
+  });
+  return { imageUrls, info, comments };
+};
+
+const getLinkChapters = async (url: string) => {
   let chapters: ILinkChapter[] = [];
-  // $(".reading .container .top")
-  //   .first()
-  //   .each(function (index, element) {
-  //     const originalUrl = PATH.netTruyen + "/";
-  //     const heading = $(element).find("h1.txt-primary");
-  //     const href = heading.find("a").attr("href")?.replace(originalUrl, "") as string;
-  //     const title = heading.find("a").text();
-  //     const chapter = heading.find("span").text()?.replace("- ", "") as string;
-  //     const updatedAt = $(element).find("i").text();
-  //     info = { title, updatedAt, chapter, href };
-  //   });
-  // $(".reading-detail .page-chapter").each(function (index, element) {
-  //   const imageUrl = getImagesReading($(element));
-  //   imageUrls.push(imageUrl);
-  // });
-  // $(".comment-list .item.clearfix").each(function (index, element) {
-  //   const comment = crawlComments($(element), $);
-  //   comments.push(comment);
-  // });
-  // const response2 = await axios.get(
-  //   `${PATH.netTruyenComic}/${info.href?.replace("truyen-tranh/", "")}`
-  // );
-  const response2 = await axios.get(
-    `http://nhattruyenone.com/truyen-tranh/vuong-gia-cuong-ghen-tuong-69754`
-  );
-  const html2 = response2.data;
-  const $2 = cheerio.load(html2);
-  $2("#ctl00_divCenter .list-chapter li.row").each(function (index, element) {
-    const chapter = crawlChapters($2(element));
+  const response = await axios.get(url);
+  const html = response.data;
+  const $ = cheerio.load(html);
+  $("#ctl00_divCenter .list-chapter li.row").each(function (index, element) {
+    const chapter = crawlChapters($(element));
     chapters.push(chapter);
   });
   return { chapters };
