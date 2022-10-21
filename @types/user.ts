677b3dbca3ea2b4d5019a140746e8bfcaaca7f4f
lamhoang1256 <lamhoang1256@gmail.com>
2022-10-21 12:17:40 +0700

    Fix: conflict type of ICurrentUser of firebase

--- @types/user.ts
+++ @types/user.ts
@@ -1,11 +1,11 @@
 import { User } from "firebase/auth";
 
 export interface ICurrentUser extends User {
-  password: string;
-  role: string;
-  status: string;
-  level: number;
-  createdAt: string;
-  fullname: string;
-  gender: { value: string; label: string };
+  password?: string;
+  role?: string;
+  status?: string;
+  level?: number;
+  createdAt?: string;
+  fullname?: string;
+  gender?: { value: string; label: string };
 }
