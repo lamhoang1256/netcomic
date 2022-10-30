901e624db2b1e21eb42e4a510a1da9646756e4f2
lamhoang1256 <lamhoang1256@gmail.com>
2022-10-30 15:03:21 +0700

    Chore: add title head at _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,6 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.png" />
+        <title>Đọc truyện tranh online - Truyện gì cũng có - NetComic</title>
       </Head>
       <Authentication>
         <Component {...pageProps} />
