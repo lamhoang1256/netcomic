0f91df9e6d336fc5d2b3e79896f64e2c82a0c967
lamhoang1256 <lamhoang1256@gmail.com>
2022-10-30 21:17:40 +0700

    Chore: test SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,11 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="vi">
-      <Head>
-        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
-        <meta name="referrer" content="no-referrer" />
-      </Head>
+    <Html>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
