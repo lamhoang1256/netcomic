86e040cc6abce5187b0f3f57196da3d97a7b40bf
lamhoang1256 <lamhoang1256@gmail.com>
2022-10-30 19:58:24 +0700

    Chore: test meta tags without key

--- components/meta/Meta.tsx
+++ components/meta/Meta.tsx
@@ -18,30 +18,31 @@ const Meta = ({
     <Head>
       {/* HTML Meta Tags */}
       <title>{title}</title>
-      <meta name="title" content={title} key="title" />
-      <meta name="robots" content="index,follow" key="robots" />
-      <meta name="googlebot" content="index,follow" key="googlebot" />
-      <meta name="description" content={description} key="description" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" key="viewport" />
-      <link rel="icon" href="/favicon.png" key="favicon" />
-      <link rel="canonical" href={server} key="canonical" />
+      <meta name="title" content={title} />
+      <meta name="robots" content="index,follow" />
+      <meta name="googlebot" content="index,follow" />
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="/favicon.png" />
+      <link rel="canonical" href={server} />
       {/* Google / Search Engine Tags */}
-      <meta itemProp="name" content={title} key="gg-name" />
-      <meta itemProp="description" content={description} key="gg-description" />
-      <meta itemProp="image" content={image} key="gg-image" />
+      <meta itemProp="name" content={title} />
+      <meta itemProp="description" content={description} />
+      <meta itemProp="image" content={image} />
       {/* Facebook Meta Tags  */}
-      <meta property="og:type" content="website" key="og-type" />
-      <meta property="og:title" content={title} key="og-title" />
-      <meta property="og:description" content={description} key="og-description" />
-      <meta property="og:image" content={image} key="og-image" />
-      <meta property="og:image:alt" content={description} key="og-image-alt" />
-      <meta property="og:locale" content="vi_VN" key="og-locale" />
-      <meta property="og:url" content={`${server}${router.asPath}`} key="og-url" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="og:image:alt" content={description} />
+      <meta property="og:locale" content="vi_VN" />
+      <meta property="og:url" content={`${server}${router.asPath}`} />
       {/* Twitter Meta Tags */}
-      <meta property="twitter:card" content="summary_large_image" key="twitter-card" />
-      <meta property="twitter:title" content={title} key="twitter-title" />
-      <meta property="twitter:description" content={description} key="twitter-desc" />
-      <meta property="twitter:image" content={image} key="twitter-image" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={`${server}${router.asPath}`} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
     </Head>
   );
 };
