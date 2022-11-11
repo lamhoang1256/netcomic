39f388362d9fef7915634f5468f8208307bebd66
lamhoang1256 <lamhoang1256@gmail.com>
2022-11-11 16:06:18 +0700

    Chore: axiosNhattruyen add withCredentials

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -4,6 +4,7 @@ const BASE_URL_API = process.env.NEXT_PUBLIC_NHATTRUYEN as string;
 
 const axiosNhattruyen = axios.create({
   baseURL: `https://corsproxy.io/?${encodeURIComponent(BASE_URL_API)}`,
+  withCredentials: false,
   headers: {
     Accept: "application/json",
     "User-Agent": "axios 0.21.1",
