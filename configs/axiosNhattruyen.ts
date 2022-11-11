398fcd3f758dedf1042409711c916d4edb0f8073
lamhoang1256 <lamhoang1256@gmail.com>
2022-11-11 15:24:03 +0700

    Chore: test home api

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -5,8 +5,8 @@ const BASE_URL_API = process.env.NEXT_PUBLIC_NHATTRUYEN as string;
 const axiosNhattruyen = axios.create({
   baseURL: `https://corsproxy.io/?${encodeURIComponent(BASE_URL_API)}`,
   headers: {
-    origin: process.env.NEXT_PUBLIC_NHATTRUYEN as string,
-    referer: process.env.NEXT_PUBLIC_NHATTRUYEN as string,
+    origin: "https://www.nettruyenin.com",
+    referer: "https://www.nettruyenin.com",
   },
 });
 
