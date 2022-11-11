c926bafc678d9532f179199b70a0eabab6cf14fa
lamhoang1256 <lamhoang1256@gmail.com>
2022-11-11 15:15:10 +0700

    Chore: update home api

--- pages/api/home.ts
+++ pages/api/home.ts
@@ -24,33 +24,37 @@ const HomePageApi = async (req: NextApiRequest, res: NextApiResponse) => {
 };
 
 async function crawlHomePage(query: Partial<IQueryParams>) {
-  const response = await axios.get("https://corsproxy.io/?https%3A%2F%2Fnhattruyenin.com", {
-    params: query,
-  });
-  const html = response.data;
-  const $ = cheerio.load(html);
-  let banners: IBanner[] = [];
-  let newestComics: IComic[] = [];
-  let paginations: IPagination[] = [];
-  let chartRankings: IComicChartRanking[] = [];
-  $("#ctl00_divAlt1 .items-slide .item", html).each(function (index, element) {
-    const banner = crawlBanner($(element));
-    banners.push(banner);
-  });
-  $("#ctl00_divCenter .ModuleContent .item", html).each(function (index, element) {
-    const comic = crawlComic($(element), $);
-    newestComics.push(comic);
-  });
-  $("#ctl00_divCenter .pagination li", html).each(function (index, element) {
-    const pagination = crawlPagination($(element));
-    paginations.push(pagination);
-  });
-  $("#ctl00_divRight #topMonth li", html).each(function (index, element) {
-    const comic = crawlComicTopMonth($(element));
-    chartRankings.push(comic);
-  });
-  // const categories = await crawlCategories({});
-  return { banners, newestComics, paginations, chartRankings };
+  try {
+    const response = await axios.get("https://corsproxy.io/?https%3A%2F%2Fnhattruyenin.com", {
+      params: query,
+    });
+    const html = response.data;
+    const $ = cheerio.load(html);
+    let banners: IBanner[] = [];
+    let newestComics: IComic[] = [];
+    let paginations: IPagination[] = [];
+    let chartRankings: IComicChartRanking[] = [];
+    $("#ctl00_divAlt1 .items-slide .item", html).each(function (index, element) {
+      const banner = crawlBanner($(element));
+      banners.push(banner);
+    });
+    $("#ctl00_divCenter .ModuleContent .item", html).each(function (index, element) {
+      const comic = crawlComic($(element), $);
+      newestComics.push(comic);
+    });
+    $("#ctl00_divCenter .pagination li", html).each(function (index, element) {
+      const pagination = crawlPagination($(element));
+      paginations.push(pagination);
+    });
+    $("#ctl00_divRight #topMonth li", html).each(function (index, element) {
+      const comic = crawlComicTopMonth($(element));
+      chartRankings.push(comic);
+    });
+    // const categories = await crawlCategories({});
+    return { banners, newestComics, paginations, chartRankings };
+  } catch (error) {
+    return error;
+  }
 }
 
 export default catchAsync(HomePageApi);
