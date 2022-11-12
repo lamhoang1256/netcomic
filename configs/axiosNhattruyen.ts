91c176e0cd3603020b53288204d7ae51d5a9e502
lamhoang1256 <lamhoang1256@gmail.com>
2022-11-12 16:36:19 +0700

    Chore: test axiosNhattruyen

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -3,21 +3,10 @@ import axios from "axios";
 const BASE_URL_API = process.env.NEXT_PUBLIC_NHATTRUYEN as string;
 
 const axiosNhattruyen = axios.create({
-  withCredentials: false,
+  baseURL: BASE_URL_API,
   headers: {
     Accept: "application/json",
-    "User-Agent": "axios 0.21.1",
   },
 });
-
-axiosNhattruyen.interceptors.request.use(
-  async (config) => {
-    console.log("config: ", config);
-    return { ...config, url: `https://corsproxy.io/?${encodeURIComponent(BASE_URL_API)}` };
-  },
-  (err) => {
-    return Promise.reject(err);
-  }
-);
 
 export default axiosNhattruyen;
