f2b3ce94d57c2d215a7a705c1a5f683145dda26b
lamhoang1256 <lamhoang1256@gmail.com>
2022-11-12 20:16:59 +0700

    Fix: mistake / at baseURL axiosNhattruyen

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL_API = process.env.NEXT_PUBLIC_NHATTRUYEN as string;
 
 const axiosNhattruyen = axios.create({
-  baseURL: "https://nct.napdev.workers.dev" + BASE_URL_API,
+  baseURL: "https://nct.napdev.workers.dev/" + BASE_URL_API,
   headers: {
     Accept: "application/json",
   },
