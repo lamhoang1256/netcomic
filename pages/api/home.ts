d6a8595f88adad7b3416f165cc925db7baa86e44
Nguyen Hoang Lam <lamhoang1256@gmail.com>
2022-11-17 23:28:04 +0700

    Chore: update path crawl home api

--- pages/api/home.ts
+++ pages/api/home.ts
@@ -26,7 +26,7 @@ const HomePageApi = async (req: NextApiRequest, res: NextApiResponse) => {
 
 async function crawlHomePage(query: Partial<IQueryParams>) {
   try {
-    const response = await axiosNhattruyen.post("/");
+    const response = await axiosNhattruyen.post(PATH.nhatTruyen);
     const html = response.data;
     const $ = cheerio.load(html);
     let banners: IBanner[] = [];
