68037c3fb51e3694c1a3e18e5b252e05799c5703
Nguyen Hoang Lam <lamhoang1256@gmail.com>
2022-11-17 23:23:14 +0700

    Test: use method Post for home api axiosNhattruyen

--- pages/api/home.ts
+++ pages/api/home.ts
@@ -26,7 +26,7 @@ const HomePageApi = async (req: NextApiRequest, res: NextApiResponse) => {
 
 async function crawlHomePage(query: Partial<IQueryParams>) {
   try {
-    const response = await axios.get("https://nct.napdev.workers.dev/" + PATH.nhatTruyen);
+    const response = await axiosNhattruyen.post("/");
     const html = response.data;
     const $ = cheerio.load(html);
     let banners: IBanner[] = [];
