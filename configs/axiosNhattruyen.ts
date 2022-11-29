57b0f51f27815c90173555020373df9b3eab468c
lamhoang1256 <lamhoang1256@gmail.com>
2022-11-29 08:16:18 +0700

    Chore: remove config axiosNhattruyen

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -1,21 +1,27 @@
 import axios from "axios";
-import queryString from "query-string";
 
-const axiosNhattruyen = axios.create({
-  baseURL: "https://nct.napdev.workers.dev",
-});
-
-axiosNhattruyen.interceptors.request.use(
-  async (config) => {
-    if (!config.params) return { ...config, url: `/${config.url}` };
-    const strParams = queryString.stringify(config.params);
-    delete config.params;
-    const customConfigs = { ...config, url: `/${config.url}?${strParams}` };
-    return customConfigs;
-  },
-  (err) => {
-    return Promise.reject(err);
-  }
-);
+const axiosNhattruyen = axios.create({});
 
 export default axiosNhattruyen;
+
+// import axios from "axios";
+// import queryString from "query-string";
+
+// const axiosNhattruyen = axios.create({
+//   baseURL: "https://nct.napdev.workers.dev",
+// });
+
+// axiosNhattruyen.interceptors.request.use(
+//   async (config) => {
+//     if (!config.params) return { ...config, url: `/${config.url}` };
+//     const strParams = queryString.stringify(config.params);
+//     delete config.params;
+//     const customConfigs = { ...config, url: `/${config.url}?${strParams}` };
+//     return customConfigs;
+//   },
+//   (err) => {
+//     return Promise.reject(err);
+//   }
+// );
+
+// export default axiosNhattruyen;
