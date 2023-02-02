d64657568aa2218236a4fec976c792b04e481210
lamhoang1256 <lamhoang1256@gmail.com>
2023-02-02 16:15:08 +0700

    Chore: change ?url to ?destination

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import queryString from "query-string";
 
 const axiosNhattruyen = axios.create({
-  baseURL: "https://proxy.braflix.workers.dev/?destination="
+  baseURL: "https://proxy.braflix.workers.dev"
 });
 
 axiosNhattruyen.interceptors.request.use(
   async (config) => {
-    if (!config.params) return { ...config, url: `?url=${config.url}` };
+    if (!config.params) return { ...config, url: `?destination=${config.url}` };
     const strParams = queryString.stringify(config.params);
     delete config.params;
     const customConfigs = { ...config, url: `?url=${config.url}?${strParams}` };
