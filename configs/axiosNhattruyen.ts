27a77a8ca33f07f4c8c01a95df4cd6eb526b98d1
Nguyen Hoang Lam <lamhoang1256@gmail.com>
2023-03-04 17:38:54 +0700

    Chore: Update cors proxy url

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import queryString from "query-string";
 
 const axiosNhattruyen = axios.create({
-  baseURL: "https://proxy.braflix.workers.dev"
+  baseURL: "https://corsproxy.io/?"
 });
 
 axiosNhattruyen.interceptors.request.use(
   async (config) => {
-    if (!config.params) return { ...config, url: `?destination=${config.url}` };
+    if (!config.params) return { ...config, url: `?${config.url}` };
     const strParams = queryString.stringify(config.params);
     delete config.params;
     const customConfigs = { ...config, url: `?url=${config.url}?${strParams}` };
